bdb833f7967b5074c023a953e58b5a3ac866d92f
afzalidny <www.khazaei1370@gmail.com>
2021-09-21 09:03:27 +0430

    add sample project by using javascript

---
+++ phonebook 2/src/app/partials/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Header = () => {
+    return <div><i className="fa fa-address-book" aria-hidden="true"></i><h1 className='name-of-app'>Phone Book App</h1></div>
+};
+
+export default Header;
